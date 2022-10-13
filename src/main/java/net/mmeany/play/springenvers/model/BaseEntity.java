5e2cab96dbead6124be49c202e150af8193d5596
Mark Meany <mmeany@users.noreply.github.com>
2022-10-13 10:10:06 +0100

    Simplify Envers configuration, remove global entity listener configuration (#1)

--- src/main/java/net/mmeany/play/springenvers/model/BaseEntity.java
+++ src/main/java/net/mmeany/play/springenvers/model/BaseEntity.java
@@ -10,18 +10,20 @@ import org.springframework.data.annotation.CreatedBy;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedBy;
 import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import javax.persistence.*;
 import java.util.Date;
 
 @MappedSuperclass
 @Audited
+@EntityListeners(AuditingEntityListener.class)
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
 @SuperBuilder(toBuilder = true)
-public class BaseEntity {
+public abstract class BaseEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
--- src/main/java/net/mmeany/play/springenvers/model/Note.java
+++ src/main/java/net/mmeany/play/springenvers/model/Note.java
@@ -1,23 +1,35 @@
 package net.mmeany.play.springenvers.model;
 
-import lombok.AccessLevel;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-import lombok.experimental.SuperBuilder;
+import lombok.*;
+import org.hibernate.Hibernate;
 import org.hibernate.envers.Audited;
 
 import javax.persistence.Entity;
 import javax.persistence.Table;
+import java.util.Objects;
 
 @Entity
 @Table(name = "note")
 @Audited
-@Data
+@Getter
+@Setter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor(access = AccessLevel.PROTECTED)
-@SuperBuilder
+@Builder
 public class Note extends BaseEntity {
     private String title;
     private String description;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || Hibernate.getClass(this) != Hibernate.getClass(o)) return false;
+        Note note = (Note) o;
+        return getId() != null && Objects.equals(getId(), note.getId());
+    }
+
+    @Override
+    public int hashCode() {
+        return getClass().hashCode();
+    }
 }
